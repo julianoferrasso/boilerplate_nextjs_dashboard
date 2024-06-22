a59310feb5bc8efc15e620d3b62ef2cb5c5bb331
Juliano Ferrasso <julianoferrasso@gmail.com>
2024-06-21 21:41:02 -0300

    DEV: criado middleware para proteger rotas autenticadas

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 import { NextRequest } from 'next/server';
-import { parseCookies } from 'nookies';
+
+// https://nextjs.org/docs/app/building-your-application/routing/middleware
 
 export function middleware(request: NextRequest) {
     const token = request.cookies.get('boilerplateNext_token');
