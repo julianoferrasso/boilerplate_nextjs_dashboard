a8e056999e1c762a77f68642516d456c08a11911
julianoferrasso <julianoferrasso@gmail.com>
2024-07-15 12:33:31 -0300

    REFACT: exclude CSP from next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = {
-    async headers() {
-        return [
-            {
-                source: '/(.*)',
-                headers: [
-                    {
-                        key: 'Content-Security-Policy',
-                        value: "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; font-src 'self' data:; img-src 'self' data:; connect-src 'self';",
-                    },
-                ],
-            },
-        ]
-    },
-}
-
-
 export default nextConfig;
