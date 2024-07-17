8abeff919908a40056e033ab695f2f0d9390dea6
Juliano Ferrasso <julianoferrasso@gmail.com>
2024-07-16 22:43:31 -0300

    DEV: Inicado Landing Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,10 +36,17 @@ export default function Component() {
           </nav>
           <Link
             href="/signUp"
-            className="inline-flex h-9 items-center justify-center rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+            className="inline-flex h-9 items-center justify-center rounded-md bg-bg-primary px-4 py-2 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
             prefetch={false}
           >
             Crie sua conta
+          </Link>
+          <Link
+            href="//auth"
+            className="inline-flex h-9 items-center justify-center rounded-md bg-bg-secondary px-4 py-2 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+            prefetch={false}
+          >
+            Login
           </Link>
         </div>
       </header>
